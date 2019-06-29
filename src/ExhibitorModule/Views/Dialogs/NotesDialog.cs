6c121367b83f3830981fb27106f4fff8edc49c5a
Hussain N Abbasi <hnabbasi@outlook.com>
2019-06-29 17:04:21 -0500

    Notes page UI updates

--- src/ExhibitorModule/Views/Dialogs/NotesDialog.cs
+++ src/ExhibitorModule/Views/Dialogs/NotesDialog.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using ExhibitorModule.Controls;
-using ExhibitorModule.Models;
+﻿using ExhibitorModule.Controls;
+using ExhibitorModule.Controls.Converters;
+using FFImageLoading.Forms;
+using FFImageLoading.Transformations;
 using IntelliAbb.Xamarin.Controls;
-using Prism.Services.Dialogs;
 using Prism.Services.Dialogs.Xaml;
 using Xamarin.Forms;
 
@@ -11,14 +11,13 @@ namespace ExhibitorModule.Views
     public class NotesDialog : ContentView
     {
         private CardView _cardView;
-        private Label _title;
-        private Editor _editor;
-        private XDSButton _saveButton;
-        private ActivityIndicator _activityIndicator;
+        private readonly Style _labelStyle;
+        private readonly Style _labelValueStyle;
 
         public NotesDialog()
         {
             Content = BuildView();
+            _labelStyle = GetLabelStyle();
 
             _cardView.TranslationY = 100;
 
@@ -27,67 +26,129 @@ namespace ExhibitorModule.Views
             _cardView.TranslateTo(x, y, easing: Easing.SpringOut);
         }
 
+        Style GetLabelStyle()
+        {
+            return new Style(typeof(Label))
+            {
+                Setters =
+                {
+                    new Setter { Property = Label.TextColorProperty, Value = Color.Silver },
+                    new Setter { Property = Label.FontSizeProperty, Value = 9 }
+                }
+            };
+        }
+
         private View BuildView()
         {
-            _activityIndicator = new ActivityIndicator
+            var activityIndicator = new ActivityIndicator
             {
                 HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.Center
             };
-            _activityIndicator.SetBinding(IsVisibleProperty, new Binding("IsBusy"));
+            activityIndicator.SetBinding(IsVisibleProperty, new Binding("IsBusy"));
 
-            _title = new Label
+            var company = new Label
             {
-                FontSize = 14,
-                HorizontalOptions = LayoutOptions.FillAndExpand
+                Style = _labelValueStyle,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Margin = new Thickness(8,0)
             };
-            _title.SetBinding(Label.TextProperty, new Binding("CurrentLead.FirstName", stringFormat: "Notes for {0}"));
+            company.SetBinding(Label.TextProperty, new Binding("CurrentLead.Company"));
 
-            _editor = new Editor
+            var email = new Label
             {
-                MinimumHeightRequest = 150,
+                Style = _labelValueStyle,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Margin = new Thickness(8, 0)
+            };
+            email.SetBinding(Label.TextProperty, new Binding("CurrentLead.Email"));
+
+            var editor = new Editor
+            {
+                HeightRequest = 200,
                 VerticalOptions = LayoutOptions.FillAndExpand,
                 Keyboard = Keyboard.Text
             };
-            _editor.SetBinding(Editor.TextProperty, new Binding("CurrentLead.Notes", BindingMode.TwoWay));
+            editor.SetBinding(Editor.TextProperty, new Binding("CurrentLead.Notes", BindingMode.TwoWay));
 
-            _saveButton = new XDSButton
+            var saveButton = new XDSButton
             {
                 Text = "Save"
             };
-            _saveButton.SetBinding(Button.CommandProperty, new Binding("SaveCommand"));
+            saveButton.SetBinding(Button.CommandProperty, new Binding("SaveCommand"));
 
+            var avatar = new CachedImage {
+                HeightRequest = 100,
+                WidthRequest = 100,
+                Aspect = Aspect.AspectFit
+            };
+            avatar.Transformations.Add(new CircleTransformation());
+            avatar.SetBinding(CachedImage.SourceProperty, new Binding("CurrentLead.GravatarUri",
+                                                                    converter: new GravatarConverter(),
+                                                                    converterParameter:100));
+
+            var firstNameSpan = new Span();
+            firstNameSpan.SetBinding(Span.TextProperty, new Binding("CurrentLead.FirstName", BindingMode.OneWay, stringFormat:"{0} "));
+            var lastNameSpan = new Span();
+            lastNameSpan.SetBinding(Span.TextProperty, new Binding("CurrentLead.LastName", BindingMode.OneWay));
+            
+            var name = new Label {
+                FontSize = 24,
+                TextColor = Color.Black,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.End,
+                Margin = new Thickness(8,0),
+                FormattedText = new FormattedString {
+                        Spans = {
+                            firstNameSpan,
+                            lastNameSpan
+                        }
+                }
+            };
+
+            var mainGrid = new Grid();
+            mainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+            mainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            
+            mainGrid.Children.Add(avatar);
+            mainGrid.Children.Add(
+                new StackLayout {
+                        Spacing = 0,
+                        Children = {
+                            name,
+                            company,
+                            email,
+                            new StackLayout
+                            {
+                                HeightRequest = 32.0,
+                                Orientation = StackOrientation.Horizontal,
+                                Children =
+                                {
+                                    new Label
+                                    {
+                                        Text = "Notes",
+                                        Style = _labelStyle,
+                                        VerticalOptions = LayoutOptions.End
+                                    },
+                                    activityIndicator
+                                }
+                            },
+                            editor,
+                            saveButton
+                        }
+                }, 0, 1
+            );
             _cardView = new CardView
             {
                 Visual = VisualMarker.Material,
                 Padding = 16,
-                Content = new StackLayout {
-                    Children = {
-                        new StackLayout
-                        {
-                            HeightRequest = 32.0,
-                            Orientation = StackOrientation.Horizontal,
-                            Children =
-                            {
-                                _title,
-                                _activityIndicator
-                            }
-                        },
-                        _editor,
-                        _saveButton
-                    }
-                },
-                HeightRequest = 200
+                Content = new ScrollView { Content = mainGrid },
+                MinimumHeightRequest = 300
             };
 
             DialogLayout.SetRelativeWidthRequest(this, 0.75);
             return _cardView;
-        }
-
-        protected override void OnBindingContextChanged()
-        {
-            base.OnBindingContextChanged();
-            var x = BindingContext;
         }
     }
 }
